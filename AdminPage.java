10048708b8e5111439025795b31a27b82da9ade8
tanya1998 <tanya16108@iiitd.ac.in>
2017-11-04 10:11:21 +0530

    Update 2

--- AdminPage.java
+++ AdminPage.java
@@ -3,12 +3,21 @@ package GUIComponents;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import application.Admin;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class AdminPage  implements javafx.fxml.Initializable {
 	@FXML
@@ -26,10 +35,20 @@ public class AdminPage  implements javafx.fxml.Initializable {
 	Pane ManagePane, Default,ChangePasswordPane;
 	
 	@FXML
-	Button showB,profB,bookB,CancelB,RequestB,ManageB,ChangePassword,Changed;
-	
+	Button showB,profB,bookB,CancelB,RequestB,ManageB,ChangePassword,Changed,LogoutBtn;
+	@FXML
+	TextField TXTnewpwd1,TXTnewpwd2, OldPwd;
+	@FXML
+	Label Lblemail, LblName;
+	static Admin admin;
+	public void setAdmin(Admin a)
+	{
+		admin = a;
+	}
 	@Override	
+	
 	public void initialize(URL location, ResourceBundle resources) {
+		
 		
 		showB.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -69,6 +88,9 @@ public class AdminPage  implements javafx.fxml.Initializable {
 			
 			@Override
 			public void handle(ActionEvent event) {
+				LblName.setText("Hi ! "+admin.getName()+"!!");
+				Lblemail.setText(admin.getemail());
+				
 				ProfilePane.setVisible(true);
 				ShowPane.setVisible(false);
 				BookPane.setVisible(false);
@@ -152,6 +174,28 @@ public class AdminPage  implements javafx.fxml.Initializable {
 			
 			@Override
 			public void handle(ActionEvent event) {
+				String np1=TXTnewpwd1.getText();
+				String np2=TXTnewpwd2.getText();
+				String op = OldPwd.getText();
+				if(np1.equals(np2) && admin.AuthenticateUser(admin.getemail(), op))
+				{
+					admin.changePassword(np1);
+					JOptionPane.showMessageDialog(null, "Password Changed Succesfully");
+				}
+				else
+				if(!admin.AuthenticateUser(admin.getemail(), op))
+				{
+					JOptionPane.showMessageDialog(null, "Your Old Password is incorrect. Please Try Again!");
+					OldPwd.setText("");
+					TXTnewpwd1.setText("");
+					TXTnewpwd2.setText("");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Please make sure that your new passwords match");
+					TXTnewpwd1.setText("");
+					TXTnewpwd2.setText("");
+				}
 				BookPane.setVisible(false);
 				ShowPane.setVisible(false);
 				ProfilePane.setVisible(true);
@@ -161,6 +205,24 @@ public class AdminPage  implements javafx.fxml.Initializable {
 				RequestPane.setVisible(false);
 				ManagePane.setVisible(false);
 				
+			}
+		});
+		LogoutBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				try
+				{
+				Parent page =  FXMLLoader.load(getClass().getResource("Logout.fxml"));
+				Scene scene = new Scene(page);
+				Stage page_stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+				page_stage.setScene(scene);
+				page_stage.show();
+				}
+				catch(Exception exp)
+				{
+					System.out.println(exp.getMessage());
+				}
 			}
 		});
 		
--- FacultyPage.java
+++ FacultyPage.java
@@ -5,12 +5,22 @@ package GUIComponents;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import application.Admin;
+import application.Faculty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class FacultyPage  implements javafx.fxml.Initializable {
 	@FXML
@@ -23,8 +33,16 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 	Pane CancelPane,Default,ChangePasswordPane;
 	
 	@FXML
-	Button showB,profB,bookB,CancelB,ChangePassword,Changed;
-	
+	Button showB,profB,bookB,CancelB,ChangePassword,Changed,LogoutBtn;
+	@FXML
+	TextField TXTnewpwd1,TXTnewpwd2, OldPwd;
+	@FXML
+	Label Lblemail, LblName;
+	static Faculty faculty;
+	public void setFaculty(Faculty f)
+	{
+		faculty = f;
+	}
 	@Override	
 	public void initialize(URL location, ResourceBundle resources) {
 		
@@ -37,7 +55,6 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 				ProfilePane.setVisible(false);
 				CancelPane.setVisible(false);
 				Default.setVisible(false);
-				ChangePasswordPane.setVisible(false);
 				
 			}
 			
@@ -47,12 +64,13 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 			
 			@Override
 			public void handle(ActionEvent event) {
+				LblName.setText("Hi ! "+faculty.getName()+"!!");
+				Lblemail.setText(faculty.getemail());
 				ProfilePane.setVisible(true);
 				ShowPane.setVisible(false);
 				BookPane.setVisible(false);
 				CancelPane.setVisible(false);
 				Default.setVisible(false);
-				ChangePasswordPane.setVisible(false);
 
 			}
 			
@@ -67,7 +85,6 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 				BookPane.setVisible(true);
 				CancelPane.setVisible(false);
 				Default.setVisible(false);
-				ChangePasswordPane.setVisible(false);
 			}
 			
 		});	
@@ -81,12 +98,10 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 				ProfilePane.setVisible(false);
 				CancelPane.setVisible(true);
 				Default.setVisible(false);
-				ChangePasswordPane.setVisible(false);
 			
 				
 			}
 		});
-		
 		ChangePassword.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -105,6 +120,28 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 			
 			@Override
 			public void handle(ActionEvent event) {
+				String np1=TXTnewpwd1.getText();
+				String np2=TXTnewpwd2.getText();
+				String op = OldPwd.getText();
+				if(np1.equals(np2) && faculty.AuthenticateUser(faculty.getemail(), op))
+				{
+					faculty.changePassword(np1);
+					JOptionPane.showMessageDialog(null, "Password Changed Succesfully");
+				}
+				else
+				if(!faculty.AuthenticateUser(faculty.getemail(), op))
+				{
+					JOptionPane.showMessageDialog(null, "Your Old Password is incorrect. Please Try Again!");
+					OldPwd.setText("");
+					TXTnewpwd1.setText("");
+					TXTnewpwd2.setText("");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Please make sure that your new passwords match");
+					TXTnewpwd1.setText("");
+					TXTnewpwd2.setText("");
+				}
 				BookPane.setVisible(false);
 				ShowPane.setVisible(false);
 				ProfilePane.setVisible(true);
@@ -116,8 +153,26 @@ public class FacultyPage  implements javafx.fxml.Initializable {
 			}
 		});
 		
-		
+		LogoutBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				try
+				{
+				Parent page =  FXMLLoader.load(getClass().getResource("Logout.fxml"));
+				Scene scene = new Scene(page);
+				Stage page_stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+				page_stage.setScene(scene);
+				page_stage.show();
+				}
+				catch(Exception exp)
+				{
+					System.out.println(exp.getMessage());
+				}
+			}
+		});
+			}
 	}	
 	
 
-}
+
