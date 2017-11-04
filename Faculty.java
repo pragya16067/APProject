db98e941bed8da59e7c274e5e947deef554faad6
tanya1998 <tanya16108@iiitd.ac.in>
2017-11-04 10:12:42 +0530

    Update 1

---
+++ Faculty.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Faculty extends User{
+	
+	public Faculty(String e, String pwd, String t) {
+		super(e,pwd,"Faculty");
+	}
+	
+	public String getName() {
+		String e=this.email;
+		String[] s=e.split("@");
+		String Name=s[0];
+		return Name.toUpperCase();
+	}
+	
+	
+	public void changePassword(String s) {
+		String e=this.email;
+		try
+		{
+			Class.forName("java.sql.DriverManager");
+	        Connection con=(Connection) DriverManager.getConnection(
+	                "jdbc:mysql://localhost:3306/project","root","30july1998");
+	        Statement stmt=(Statement) con.createStatement();
+	        String q="update users set password='"+s+"'where email='"+e+"';";
+	        stmt.executeUpdate(q);
+	        
+		}
+		catch(Exception exp)
+		{
+			System.out.println(exp.getMessage());
+		}
+		
+	}
+	
+	public ResultSet ViewRoomBookings() {
+		ResultSet rs=null;
+		try
+		{
+			Class.forName("java.sql.DriverManager");
+	        Connection con=(Connection) DriverManager.getConnection(
+	                "jdbc:mysql://localhost:3306/project","root","30july1998");
+	        Statement stmt=(Statement) con.createStatement();
+	        String q="Select password from users where email='"+this.email+"';";
+	        rs=stmt.executeQuery(q);
+	        return rs;
+		}
+		catch(Exception exp)
+		{
+			System.out.println(exp.getMessage());
+		}
+		return rs;
+	}
+}
